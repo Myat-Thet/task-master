8ff4500da5e3fd4d8b6cefa59ccb7990c2534712
myat thet <myat.mtw@gmail.com>
2025-06-14 15:09:47 +0630

    first commit

--- src/test/java/com/example/taskmaster/TaskMasterApplicationTests.java
+++ src/test/java/com/example/taskmaster/TaskMasterApplicationTests.java
@@ -1,13 +1,11 @@
 package com.example.taskmaster;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TaskMasterApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
